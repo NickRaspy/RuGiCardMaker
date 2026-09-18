35c14025aed88d46afedaae7a9c2d872f024dd43
NickRaspy <nickraspyofficial@gmail.com>
2026-09-18 12:44:52 +0500

    Init

---
+++ RuGiCardMaker/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace RuGiCardMaker;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
