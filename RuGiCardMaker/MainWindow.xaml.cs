be16fa3391a2d5967cb829417c7c6f723f30c986
NickRaspy <nickraspyofficial@gmail.com>
2026-09-19 04:24:52 +0500

    Interface prototype

--- RuGiCardMaker/MainWindow.xaml.cs
+++ RuGiCardMaker/MainWindow.xaml.cs
@@ -1,23 +1,59 @@
-﻿using System.Text;
+using Microsoft.Win32;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RuGiCardMaker;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void ChooseArtwork_Click(object sender, RoutedEventArgs e)
+    {
+        var dialog = new OpenFileDialog
+        {
+            Title = "Выберите изображение для образца карты",
+            Filter = "Изображения (*.png;*.jpg;*.jpeg)|*.png;*.jpg;*.jpeg"
+        };
+
+        if (dialog.ShowDialog(this) != true)
+            return;
+
+        try
+        {
+            var image = new BitmapImage();
+            image.BeginInit();
+            image.CacheOption = BitmapCacheOption.OnLoad;
+            image.UriSource = new Uri(dialog.FileName);
+            image.EndInit();
+            CardArtwork.Source = image;
+            CardArtwork.Visibility = Visibility.Visible;
+            ArtworkFileLabel.Text = Path.GetFileName(dialog.FileName);
+        }
+        catch (Exception)
+        {
+            MessageBox.Show(this, "Не удалось открыть изображение. Выберите файл PNG или JPG.",
+                "Ошибка изображения", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+    }
+
+    private void ResetFields_Click(object sender, RoutedEventArgs e)
+    {
+        CardNameInput.Text = "Голубоглазый белый дракон";
+        CardTypeInput.SelectedIndex = 0;
+        CardSubtypeInput.SelectedIndex = 0;
+        AttributeInput.SelectedIndex = 0;
+        LevelInput.SelectedIndex = 7;
+        RarityInput.SelectedIndex = 0;
+        AttackInput.Text = "3000";
+        DefenseInput.Text = "2500";
+        EffectInput.Text = "Легендарный дракон, чьи крылья сияют в ночном небе.";
+        CardArtwork.Source = null;
+        CardArtwork.Visibility = Visibility.Collapsed;
+        ArtworkFileLabel.Text = "Образец карты";
     }
 }
